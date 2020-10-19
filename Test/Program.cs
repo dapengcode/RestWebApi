2c61f4f4d6489526a86a4c1650f5e55401c94975
davidmaster <davidmaster@163.com>
2020-10-19 14:03:25 +0800

    ...

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestWebApi.Sign;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Signer.ToMD5("/api/productmonth/toplistwithmom20201019135822555");
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
